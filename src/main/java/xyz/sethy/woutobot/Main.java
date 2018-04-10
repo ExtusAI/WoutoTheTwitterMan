9c5fa99d953dc6fdc7d4783899373ffd7fe6834a
Seth McAllister <dozingducklet@gmail.com>
2018-04-10 22:31:36 +0100

    Huge cleanup

--- src/main/java/xyz/sethy/woutobot/Main.java
+++ src/main/java/xyz/sethy/woutobot/Main.java
@@ -1,31 +1,10 @@
 package xyz.sethy.woutobot;
 
-import xyz.sethy.woutobot.dto.OptimizedString;
-
 /**
  * Created by Seth on 30/08/2017.
  */
 public class Main {
     public static void main(String[] args) {
-//        new WoutoBot();
-
-        int i = 0;
-        long start = System.currentTimeMillis();
-        char[] same = new char[]{"a".toCharArray()[0], "b".toCharArray()[0]};
-        while(i < 100000) {
-            OptimizedString string = OptimizedString.valueOf(same);
-            i++;
-        }
-        System.out.println("toke " + (System.currentTimeMillis() - start) + "ms for 100000 optimized strings");
-
-        int i1 = 0;
-        long start1 = System.currentTimeMillis();
-        while(i1 < 100000) {
-            String string = String.valueOf(same);
-            i1++;
-        }
-
-        System.out.println("toke " + (System.currentTimeMillis() - start1) + "ms for 100000 strings");
-
+        new WoutoBot();
     }
 }
--- src/main/java/xyz/sethy/woutobot/WoutoBot.java
+++ src/main/java/xyz/sethy/woutobot/WoutoBot.java
@@ -4,14 +4,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
-import xyz.sethy.woutobot.dto.TextError;
-import xyz.sethy.woutobot.dto.TextResult;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -72,85 +64,19 @@ public class WoutoBot {
         return tweet.toString();
     }
 
-//    private String makeTweet() throws TwitterException {
-//        System.out.println("woutify start");
-//        ResponseList<Status> statuses = twitter.getHomeTimeline(new Paging(1, 40));
-//        System.out.println("str builder");
-//        StringBuilder tweet = new StringBuilder();
-//        int max = 100;
-//        final int[] i = {0};
-//
-//        System.out.println("status");
-//        statuses.forEach(s -> {
-//            if(max < i[0]) {
-//                return;
-//            }
-//            i[0]++;
-//
-//            if(s.getUser().getName().equalsIgnoreCase("woutobot"))
-//                return;
-//
-//            String[] words = s.getText().split(" ");
-//            Integer element = new Random().nextInt(words.length);
-//            String word = words[element];
-//            Pattern p = Pattern.compile(URL_REGEX);
-//            Matcher m = p.matcher(word);
-//            if((tweet.toString().length() + word.length() < 140 && !m.find())) {
-//                tweet.append(word).append(" ");
-//            }
-//        });
-//
-//        System.out.println(tweet.toString());
-//        return tweet.toString();
-//    }
-
-    private boolean isSentance(final String text) throws MalformedURLException {
-        String urlStr = "https://api.textgears.com/check.php?text=" + text.replaceAll(" ", "+") + "&key=jLnuHTytuuT4zcDZ";
-        URL url = new URL(urlStr);
-
-        BufferedReader reader = null;
-        try {
-            reader = new BufferedReader(new InputStreamReader(url.openStream()));
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-
-        StringBuilder json = new StringBuilder();
-        String line;
-        try {
-            while((line = reader.readLine()) != null)
-                json.append(line);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-
-        System.out.println(json.toString());
-
-        TextResult result = this.gson.fromJson(json.toString(), TextResult.class);
-
-        return result.getErrors().size() < 0;
-    }
-
     public class Woutoify implements Runnable {
 
         public void run() {
             try {
                 ResponseList<Status> statuses = twitter.getHomeTimeline(new Paging(1, 40));
 
-
                 String tweet = makeTweet(statuses);
-
-                while(!isSentance(tweet)) {
-                    tweet = makeTweet(statuses);
-                }
 
                 System.out.println("i would tweet " + tweet);
 
 //                twitter.updateStatus(tweet);
 
-            } catch (TwitterException | MalformedURLException e) {
+            } catch (TwitterException e) {
                 e.printStackTrace();
             }
         }
